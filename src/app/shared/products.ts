b5941fc600d70d2842c65005de0dfc5cd16aa2fd
Edith Peschke <edith.peschke@chello.at>
2024-10-21 21:26:37 +0200

    categories adapted

--- src/app/shared/products.ts
+++ src/app/shared/products.ts
@@ -2,276 +2,206 @@ import { ProductModel } from "./products.model";
 export const product: ProductModel[] = [
 {   categoryName: "Animals",
     categoryId: 1,
-    productName: "Animals1",
+    productName: "Dog",
     cardDisp: true,
-    img:"https://cdn.pixabay.com/photo/2023/04/28/12/18/dogs-7956516_1280.jpg"
-},
-{
-    categoryName: "Animals",
-    categoryId: 1,
-    productName: "Animals2",
-    cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2024/05/18/08/16/tomcat-8769861_1280.jpg"
+    img:"images/animals/animal05.jpg"
 },
 {   categoryName: "Animals",
     categoryId: 1,
-    productName: "Animals3",
+    productName: "Monkey",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2020/02/14/15/35/dog-4848668_1280.jpg"
+    img:"images/animals/animal01.jpg"
 },
 {
     categoryName: "Animals",
     categoryId: 1,
-    productName: "Animals4",
+    productName: "Parrot",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2024/05/18/08/16/tomcat-8769861_1280.jpg"
+    img:"images/animals/animal02.jpg"
 },
 {   categoryName: "Animals",
     categoryId: 1,
-    productName: "Animals5",
+    productName: "Flamingo",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2020/02/14/15/35/dog-4848668_1280.jpg"
+    img:"images/animals/animal03.jpg"
 },
 {
     categoryName: "Animals",
     categoryId: 1,
-    productName: "Cat-Picture",
+    productName: "Toad",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2024/05/18/08/16/tomcat-8769861_1280.jpg"
+    img:"images/animals/animal05.jpg"
 },
-{   categoryName: "Weddings",
+{   categoryName: "Wedding",
     categoryId: 2,
     productName: "Wedding1",
     cardDisp: true,
-    img:"https://cdn.pixabay.com/photo/2018/09/06/01/35/wedding-3657537_1280.jpg"
+     img:"images/wedding/wedding02.jpg"
 },
 {
-    categoryName: "Weddings",
+    categoryName: "Wedding",
     categoryId: 2,
     productName: "Wedding2",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2018/09/14/18/04/wedding-3677795_1280.jpg"
+    img:"images/wedding/wedding01.jpg"
 },
-{   categoryName: "Weddings",
+{   categoryName: "Wedding",
     categoryId: 2,
     productName: "Wedding3",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2018/09/06/01/35/wedding-3657537_1280.jpg"
+    img:"images/wedding/wedding03.jpg"
 },
 {
-    categoryName: "Weddings",
+    categoryName: "Wedding",
     categoryId: 2,
     productName: "Wedding4",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2018/09/14/18/04/wedding-3677795_1280.jpg"
+    img:"images/wedding/wedding04.jpg"
 },
-{   categoryName: "Weddings",
+{   categoryName: "Wedding",
     categoryId: 2,
     productName: "Wedding5",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2018/09/06/01/35/wedding-3657537_1280.jpg"
-},
-{
-    categoryName: "Weddings",
-    categoryId: 2,
-    productName: "Wedding6",
-    cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2018/09/14/18/04/wedding-3677795_1280.jpg"
+    img:"images/wedding/wedding05.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
     productName: "Portrait1",
     cardDisp: true,
-    img:"https://cdn.pixabay.com/photo/2019/11/05/16/03/man-4603859_1280.jpg"
+    img:"images/portrait/portrait01.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
     productName: "Portrait2",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2015/11/13/12/08/person-1041904_1280.jpg"
+    img:"images/portrait/portrait02.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
     productName: "Portrait3",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2019/11/05/16/03/man-4603859_1280.jpg"
+    img:"images/portrait/portrait03.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
     productName: "Portrait4",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2015/11/13/12/08/person-1041904_1280.jpg"
+    img:"images/portrait/portrait04.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
     productName: "Portrait5",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2019/11/05/16/03/man-4603859_1280.jpg"
-},
-{
-    categoryName: "Portrait",
-    categoryId: 3,
-    productName: "Portrait6",
-    cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2015/11/13/12/08/person-1041904_1280.jpg"
+    img:"images/portrait/portrait05.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
     productName: "Landscape1",
     cardDisp: true,
-    img:"https://cdn.pixabay.com/photo/2023/08/15/19/19/butterfly-8192668_1280.jpg"
+    img:"images/landscape/landscape01.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
     productName: "Landscape2",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2022/03/21/14/25/art-7083323_1280.jpg"
+    img:"images/landscape/landscape02.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
     productName: "Landscape3",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2023/08/15/19/19/butterfly-8192668_1280.jpg"
+    img:"images/landscape/landscape03.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
     productName: "Landscape4",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2022/03/21/14/25/art-7083323_1280.jpg"
+    img:"images/landscape/landscape04.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
     productName: "Landscape5",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2023/08/15/19/19/butterfly-8192668_1280.jpg"
-},
-{
-    categoryName: "Landscape",
-    categoryId: 4,
-    productName: "Landscape6",
-    cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2022/03/21/14/25/art-7083323_1280.jpg"
+    img:"images/landscape/landscape05.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
     productName: "Street1",
     cardDisp: true,
-    img:"https://cdn.pixabay.com/photo/2018/10/06/07/27/detroit-3727417_1280.jpg"
+    img:"images/street/street01.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
     productName: "Street2",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2020/01/18/13/50/united-states-4775333_1280.jpg"
+    img:"images/street/street02.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
     productName: "Street3",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2018/10/06/07/27/detroit-3727417_1280.jpg"
+    img:"images/street/street03.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
     productName: "Street4",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2020/01/18/13/50/united-states-4775333_1280.jpg"
+    img:"images/street/street04.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
     productName: "Street5",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2018/10/06/07/27/detroit-3727417_1280.jpg"
-},
-{
-    categoryName: "Street",
-    categoryId: 5,
-    productName: "Street6",
-    cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2020/01/18/13/50/united-states-4775333_1280.jpg"
+    img:"images/street/street05.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
     productName: "Sports1",
     cardDisp: true,
-    img:"https://cdn.pixabay.com/photo/2023/10/04/14/15/man-8293794_1280.jpg"
+    img:"images/sports/sports01.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
     productName: "Sports2",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2015/09/17/17/24/museum-944497_1280.jpg"
-},
-{
-    categoryName: "Sports",
-    categoryId: 6,
-    productName: "Sports1",
-    cardDisp: true,
-    img:"https://cdn.pixabay.com/photo/2022/04/15/09/14/coffee-7133980_1280.jpg"
-},
-{
-    categoryName: "Sports",
-    categoryId: 6,
-    productName: "Sports2",
-    cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2015/09/17/17/24/museum-944497_1280.jpg"
-},
-{
-    categoryName: "Sports",
-    categoryId: 6,
-    productName: "Sports1",
-    cardDisp: true,
-    img:"https://cdn.pixabay.com/photo/2019/04/14/17/54/woman-4127336_1280.jpg"
-},
-{
-    categoryName: "Sports",
-    categoryId: 6,
-    productName: "Sports2",
-    cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2023/10/04/14/15/man-8293794_1280.jpg"
+    img:"images/sports/sports02.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
     productName: "Sports3",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2023/10/04/14/15/man-8293794_1280.jpg"
+    img:"images/sports/sports03.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
-    productName: "Sports",
+    productName: "Sports4",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2023/10/04/14/15/man-8293794_1280.jpg"
-},
-{
-    categoryName: "Sports",
-    categoryId: 6,
-    productName: "Sports5",
-    cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2023/10/04/14/15/man-8293794_1280.jpg"
+    img:"images/sports/sports04.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
     productName: "Sports6",
     cardDisp: false,
-    img:"https://cdn.pixabay.com/photo/2023/10/04/14/15/man-8293794_1280.jpg"
+    img:"images/sports/sports05.jpg"
 }
 ];
