60af63274471533b7b3695c414082ace2d20f3b6
Hermann Weinberger <miol@protonmail.com>
2024-10-21 10:54:33 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Codefactory_Project_Group_5';
+}
