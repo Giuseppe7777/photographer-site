ca7d9a513d913fc49227c4d4c509487ca275aecf
Hermann Weinberger <miol@protonmail.com>
2024-10-21 11:37:21 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from "@angular/forms";
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroHomeComponent } from './hero-home/hero-home.component';
+import { HomeComponent } from './home/home.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BioComponent } from './bio/bio.component';
+import { ContactComponent } from './contact/contact.component';
+import { AwardsComponent } from './awards/awards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroHomeComponent,
+    HomeComponent,
+    TestimonialsComponent,
+    PortfolioComponent,
+    BioComponent,
+    ContactComponent,
+    AwardsComponent
   ],
   imports: [
     BrowserModule,
