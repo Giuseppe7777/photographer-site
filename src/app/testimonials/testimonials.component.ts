ae2a90dacba51f1c2b6f004b43d30ab85318792f
Yosyp Malanka <malankajosyp@gmail.com>
2024-10-22 14:37:48 +0200

    testimonials changes

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var $: any;  
 
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
   styleUrls: ['./testimonials.component.css']
 })
+export class TestimonialsComponent implements AfterViewInit {
 
-export class TestimonialsComponent  {
-
-
+  ngAfterViewInit(): void {
+    $("#testimonial-slider").owlCarousel({
+      items: 3, 
+      margin: 10,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      responsive: {
+        0: {
+          items: 1 
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 3
+        }
+      }
+    });
+  }
 }
