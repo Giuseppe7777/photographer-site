daf960863a65e5d114e8e4f94cf49df382b28e11
Edith Peschke <edith.peschke@chello.at>
2024-10-23 13:49:41 +0200

    Portfolio adaptation+Carousel

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -10,10 +10,51 @@ import { product } from '../shared/products';
 export class DetailsComponent {
   category : any;
   array : any = [];
+  currentSlide = 1;
 
   constructor(private route:ActivatedRoute){
     this.category = this.route.snapshot.params["category"];
     this.array = product.filter(item=>item.categoryName == this.category)
     console.log(this.array);
+    setTimeout(()=>{
+      this.nextSlide();
+    }, 100)
+    
   }
+
+  showSlide(index: number): void {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
+
+    
+    slides.forEach((slide: any) => {
+      slide.style.display = 'none'; 
+    });
+    console.log(totalSlides);
+    
+    
+    if (index >= totalSlides) {
+      this.currentSlide = 0;
+    } else if (index < 0) {
+      this.currentSlide = totalSlides - 1;
+    } else {
+      this.currentSlide = index;
+    }
+
+
+
+    
+    (slides[this.currentSlide] as HTMLElement).style.display = 'block';
+  }
+
+  
+  nextSlide(): void {
+    this.showSlide(this.currentSlide + 1);
+  }
+
+  
+  prevSlide(): void {
+    this.showSlide(this.currentSlide - 1);
+  }
+
 }
--- src/app/shared/products.ts
+++ src/app/shared/products.ts
@@ -30,177 +30,177 @@ export const product: ProductModel[] = [
     categoryId: 1,
     productName: "Toad",
     cardDisp: false,
-    img:"images/animals/animal05.jpg"
+    img:"images/animals/animal04.jpg"
 },
 {   categoryName: "Wedding",
     categoryId: 2,
-    productName: "Wedding1",
+    productName: "Wedding photography",
     cardDisp: true,
-     img:"images/wedding/wedding02.jpg"
+    img:"images/wedding/wedding02.jpg"
 },
 {
     categoryName: "Wedding",
     categoryId: 2,
-    productName: "Wedding2",
+    productName: "Wedding photography",
     cardDisp: false,
     img:"images/wedding/wedding01.jpg"
 },
 {   categoryName: "Wedding",
     categoryId: 2,
-    productName: "Wedding3",
+    productName: "Wedding photography",
     cardDisp: false,
     img:"images/wedding/wedding03.jpg"
 },
 {
     categoryName: "Wedding",
     categoryId: 2,
-    productName: "Wedding4",
+    productName: "Wedding photography",
     cardDisp: false,
     img:"images/wedding/wedding04.jpg"
 },
 {   categoryName: "Wedding",
     categoryId: 2,
-    productName: "Wedding5",
+    productName: "Wedding photography",
     cardDisp: false,
     img:"images/wedding/wedding05.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
-    productName: "Portrait1",
+    productName: "Portraits",
     cardDisp: true,
     img:"images/portrait/portrait01.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
-    productName: "Portrait2",
+    productName: "Portraits",
     cardDisp: false,
     img:"images/portrait/portrait02.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
-    productName: "Portrait3",
+    productName: "Portraits",
     cardDisp: false,
     img:"images/portrait/portrait03.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
-    productName: "Portrait4",
+    productName: "Portraits",
     cardDisp: false,
     img:"images/portrait/portrait04.jpg"
 },
 {
     categoryName: "Portrait",
     categoryId: 3,
-    productName: "Portrait5",
+    productName: "Portraits",
     cardDisp: false,
     img:"images/portrait/portrait05.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
-    productName: "Landscape1",
+    productName: "Landscape impressions",
     cardDisp: true,
     img:"images/landscape/landscape01.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
-    productName: "Landscape2",
+    productName: "Landscape impressions",
     cardDisp: false,
     img:"images/landscape/landscape02.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
-    productName: "Landscape3",
+    productName: "Landscape impressions",
     cardDisp: false,
     img:"images/landscape/landscape03.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
-    productName: "Landscape4",
+    productName: "Landscape impressions",
     cardDisp: false,
     img:"images/landscape/landscape04.jpg"
 },
 {
     categoryName: "Landscape",
     categoryId: 4,
-    productName: "Landscape5",
+    productName: "Landscape impressions",
     cardDisp: false,
     img:"images/landscape/landscape05.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
-    productName: "Street1",
+    productName: "Street art",
     cardDisp: true,
     img:"images/street/street01.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
-    productName: "Street2",
+    productName: "Street art",
     cardDisp: false,
     img:"images/street/street02.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
-    productName: "Street3",
+    productName: "Street art",
     cardDisp: false,
     img:"images/street/street03.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
-    productName: "Street4",
+    productName: "Street art",
     cardDisp: false,
     img:"images/street/street04.jpg"
 },
 {
     categoryName: "Street",
     categoryId: 5,
-    productName: "Street5",
+    productName: "Street art",
     cardDisp: false,
     img:"images/street/street05.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
-    productName: "Sports1",
+    productName: "Sport events",
     cardDisp: true,
     img:"images/sports/sports01.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
-    productName: "Sports2",
+    productName: "Sport events",
     cardDisp: false,
     img:"images/sports/sports02.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
-    productName: "Sports3",
+    productName: "Sport events",
     cardDisp: false,
     img:"images/sports/sports03.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
-    productName: "Sports4",
+    productName: "Sport events",
     cardDisp: false,
     img:"images/sports/sports04.jpg"
 },
 {
     categoryName: "Sports",
     categoryId: 6,
-    productName: "Sports6",
+    productName: "Sport events",
     cardDisp: false,
     img:"images/sports/sports05.jpg"
 }
