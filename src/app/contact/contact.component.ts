71cdd397c708aaf56e833db707fec30be11e3195
Yosyp Malanka <malankajosyp@gmail.com>
2024-10-24 11:38:41 +0200

    animation contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.triggerAnimations();
+  }
+
+  triggerAnimations(): void {
+    setTimeout(() => {
+      const animatedElements = document.querySelectorAll('.animated-text, .animated-scale');
+      animatedElements.forEach((el: Element) => {
+        el.classList.add('animated-text-visible', 'animated-scale-visible');
+      });
+    }, 100);
+  }
 }
+
